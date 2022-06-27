6292a4458627f3195fae60707e597d7320e4d46b
Kunalpatil009 <009kunalpatil@gmail.com>
2022-06-27 22:07:10 +0530

    SL_Prb2_solution

---
+++ SL_Game_Optioncheck.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+public class SL_Game_Optioncheck {
+    public static void main(String[] args) {
+        Random Dice = new Random();
+        int a = Dice.nextInt(6)+1;
+        System.out.println(a);
+        if(a == 0){
+            System.out.println("Sorry No Play");
+
+        }else if(a == 2){
+            System.out.println("Oop's There is SNAKE");
+        }else if(a == 4){
+            System.out.println("Go Up..! There is Ladder");
+        }else{
+            System.out.println("Play Again");
+        }
+
+    }
+}
